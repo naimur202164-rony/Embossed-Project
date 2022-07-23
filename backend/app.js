df30da37f7acc0b7db7e6cbf416e3eb0ee98cd64
naimur <naimur202163@gmail.com>
2022-07-24 00:10:12 +0600

    basic routing added

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const connectDb = require("./config/dataBase");
+const products = require("./routes/productRoutes");
+// Midleware
 
+app.use(express.json());
 // Database Connection
 connectDb();
+
+// Routes
+app.use("/api/v1",products);
 
 module.exports = app;
